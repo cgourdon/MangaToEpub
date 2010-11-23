cb6bf05b50fcffdf66d178d54107cdd3ac90c3ca
cgourdon_cp <SND\cgourdon_cp>
2010-11-23 19:51:53 +0000

    Changed Radios For Picklists

---
+++ Trunk/Epub - Manga/Converters/TrimmingMethodToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EpubManga
+{
+    /// <summary>
+    /// Converts a TrimmingMethod enumeration to a string.
+    /// </summary>
+    public class TrimmingMethodToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TrimmingMethod? trimmingMethod = value as TrimmingMethod?;
+            if (!trimmingMethod.HasValue) return Binding.DoNothing;
+
+            return trimmingMethod.Value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string trimmingMethod = value as string;
+            if (string.IsNullOrEmpty(trimmingMethod)) return Binding.DoNothing;
+
+            return (TrimmingMethod)Enum.Parse(typeof(TrimmingMethod), trimmingMethod);
+        }
+    }
+}
